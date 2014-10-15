0f6819ba41bb580d250ee729a3d5b8ffc37b6598
pistone10 <brian.pistone90@gmail.com>
2014-10-15 16:45:39 -0700

    Added CSS, JS, etc
    -Added CSS, JS, Etc
    -Replaced Search Bar with Client Select Dropdown ~ header.php

--- header.php
+++ header.php
@@ -266,15 +266,12 @@
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
                         <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
+                            <form>
+								<select name="clients">
+									<option value="LITC">Leverage IT Consulting</option>
+								</select>
+							</form>
+							<!-- Client Selector -->
                         </li>
                         <li>
                             <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require header.php;
+require ('header.php');
 ?>
 <div id="page-wrapper">
             <div class="row">
@@ -542,5 +542,5 @@ require header.php;
             </div>
             <!-- /.row -->
 <?php
-require footer.php;
+require ('footer.php');
 ?>
