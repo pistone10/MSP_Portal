36e48f637072df0a7eab30e3305b54abb70e12aa
pistone10 <brian.pistone90@gmail.com>
2014-10-15 17:05:19 -0700

    Updated Menu

--- header.php
+++ header.php
@@ -274,88 +274,43 @@
 							<!-- Client Selector -->
                         </li>
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="flot.html">Flot Charts</a>
-                                </li>
-                                <li>
-                                    <a href="morris.html">Morris.js Charts</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="#"><i class="fa fa-edit fa-fw"></i> Office Layout</a>
                         </li>
                         <li>
-                            <a class="active" href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                            <a href="#"><i class="fa fa-edit fa-fw"></i> Network Map (Visio)</a>
                         </li>
                         <li>
-                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                            <a href="#"><i class="fa fa-edit fa-fw"></i> Active Directory Domain</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="panels-wells.html">Panels and Wells</a>
-                                </li>
-                                <li>
-                                    <a href="buttons.html">Buttons</a>
-                                </li>
-                                <li>
-                                    <a href="notifications.html">Notifications</a>
-                                </li>
-                                <li>
-                                    <a href="typography.html">Typography</a>
-                                </li>
-                                <li>
-                                    <a href="grid.html">Grid</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="#"><i class="fa fa-edit fa-fw"></i> File Shares and Printers</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level <span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                    </ul>
-                                    <!-- /.nav-third-level -->
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="#"><i class="fa fa-edit fa-fw"></i> Email System</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="blank.html">Blank Page</a>
-                                </li>
-                                <li>
-                                    <a href="login.html">Login Page</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Backup Infrastructure</a>
+                        </li>
+						<li>
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Servers & Roles</a>
+                        </li>
+						<li>
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Network Information</a>
+                        </li>
+						<li>
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Domain & DNS</a>
+                        </li>
+						<li>
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Vendors</a>
+                        </li>
+						<li>
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Passwords</a>
+                        </li>
+						<li>
+                           <a href="#"><i class="fa fa-edit fa-fw"></i> Misc</a>
                         </li>
                     </ul>
                 </div>
